6b4ba9f97c33f16247f2307862a8f2cb89bd4d99
zeyvortex <zeynep.bilen.dev@gmail.com>
2025-04-18 23:29:10 +0300

    data types notes

---
+++ # PYTHON DATA TYPES NOTES.py
@@ -0,0 +1,33 @@
+# PYTHON DATA TYPES NOTES
+
+# 1. int (integer): int stands for integer and refers to whole numbers (without any decimal point)
+x = 7
+y = 3
+print(x + y)  #10
+print(x // y)  #2
+print(x % y)  #1
+
+# 2. float (floating point number): float represents real numbers (numbers with decimal points)
+z = 5.0
+t = 2
+print(z / t)  # 2.5
+
+# 3. str (string): a str is a sequene of characters, like text. strings are surrounded by either single '' or double " " quotes
+# can contain letters, numbers, symbols, and emojis
+# strings are immutable (you cannot change them directly)
+# you can concatenate (combine), slice, and loop through strings
+text = "Python"
+print(text..uppper())  #PYTHON
+print(text.lower())  # python
+print(text[0])  # P (indexing)
+print(len(text))  # 6
+
+msg = """ this is a multiline string"""
+
+# 4. bool (boolean): nool holda one of two values; True or False
+print(5 < 3)  # False
+print(4 == 4)  # True
+
+x = 5
+print(x > 2 and x < 10)  # True
+print(not(x == 5))  # False . not inverts the result.
